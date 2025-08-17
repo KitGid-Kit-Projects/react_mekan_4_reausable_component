4f20fb5c1f2fe50931c613f6036722e29437254a
Anar Rustamov <44277604+anar1984@users.noreply.github.com>
2025-08-17 12:07:34 +0200

    App updated

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { ConfigProvider } from 'antd';
 
 // Import Ant Design's reset CSS to ensure consistent styling across browsers
 import 'antd/dist/reset.css';
-import CrudExample from './page/CrudExample';
+import CrudExample from './pages/CrudExample';
 
 // Define the main App component using TypeScript's React.FC (Function Component) type
 const App: React.FC = () => {
